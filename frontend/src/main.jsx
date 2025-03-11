f658836c605a8fa18ee62ecd2c099854d6bfd3ab
warwolf6432 <jackhowardortega@gmail.com>
2025-03-11 13:31:46 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Login from "./Login"; // Asegúrate de importar el archivo correcto
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Login /> {/* Renderiza la pantalla de Login en el inicio */}
+  </React.StrictMode>
+);
