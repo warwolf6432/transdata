61f80aea906d615736504a76783a3f685c46121a
warwolf6432 <jackhowardortega@gmail.com>
2025-03-20 11:51:42 -0500

    w

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Esto evita problemas con rutas en Netlify
+  base: "./", // Corrige rutas relativas
+  build: {
+    outDir: "dist", // Asegura que la carpeta de salida sea la correcta
+  },
 });
